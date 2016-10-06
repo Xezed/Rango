b620222aca16ca0cd70dabede2e9d6e4ae726fed
Aleksandr Zakharov <sasha750i@mail.ru>
2016-10-06 17:03:14 +0300

    before staticfiles

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Rango is working!')
+    context = {'boldmessage' : 'This is the bold message HELLO WORLD!'}
+
+    return render(request, 'rango/index.html', context)
+
+
+def about(request):
+    return HttpResponse('<a href="http://127.0.0.1:8000/">Go to the main page</a>')
--- tango_with_django/tango_with_django/settings.py
+++ tango_with_django/tango_with_django/settings.py
@@ -3,6 +3,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Templates directory
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -43,7 +47,7 @@ ROOT_URLCONF = 'tango_with_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,5 +109,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATIC_URL = '/static/'
